6772d7ca3c78aae8527822ae46604564646c8a5d
Jose Proenca <jose@p187.glmf.di.uminho.pt>
2017-07-26 16:29:56 +0100

    much better parser - should be complete

--- src/main/scala/paramConnectors/Parser.scala
+++ src/main/scala/paramConnectors/Parser.scala
@@ -1,6 +1,6 @@
 package paramConnectors
 
-import paramConnectors.analysis.Show
+import paramConnectors.analysis.{Show, Substitution}
 
 import scala.util.matching.Regex
 import scala.util.parsing.combinator._
@@ -40,111 +40,73 @@ object Parser extends RegexParsers {
     case _          => DSL.str2conn(s)
   }
 
-  /*
-  prog = conn restp
-  restp = "" | prog
-
-   conn = lit restc
-   restc = & conn | * conn | ^ bexp | restbe
-
-   bexp = name
-   restbe =
-   */
 
   def conn: Parser[Connector] =
     lit ~ combinator ^^ {case l ~ f => f(l) }
-
 
   def combinator: Parser[Connector => Connector] =
     "&" ~ conn   ^^ {case _~ c => (_:Connector) & c} |
     "*" ~ conn   ^^ {case _~ c => (_:Connector) * c} |
     "^" ~ iexpr  ^^ {case _~ i => (_:Connector) ^ i} |
+    "|" ~ bexpr  ^^ {case _~ b => (_:Connector) | b} |
+    bexpr        ^^ {b => (_: Connector)(b)}         |
+    iexpr        ^^ {e => (_: Connector)(e)}         |
     ""           ^^ { _ => x:Connector => x}
-//    compBuilder | parBuilder | expBuilder | end
-  //  &            *            ^           ""
 
   // Connector Literals:
-  def lit: Parser[Connector]   = //prim | lambda | ite | brck
-    identifier                      ^^ { inferPrim }                                 |
-    bexpr ~ "?" ~ conn ~ "+" ~ conn ^^ {case b~_~c1~_~c2 => Choice(b,c1,c2)}         |
-    "\\" ~ identifier ~ lambdaCont  ^^ { case _~ s ~ cont => cont(DSL.str2IVar(s)) } |
-    "(" ~ conn ~ ")" ^^ { case _ ~ c ~ _ => c }
-//  def prim: Parser[Connector]  = identifier ^^ { inferPrim }
-//  def ite: Parser[Connector] =
-//    bexpr ~ "?" ~ conn ~ "+" ~ conn ^^ {case b~_~c1~_~c2 => Choice(b,c1,c2)}
-//  def lambda:Parser[Connector] = "\\" ~ identifier ~ lambdaCont ^^ {
-//    case _~ s ~ cont => cont(DSL.str2IVar(s))
-//  }
-  def lambdaCont:Parser[Var=>Connector] = // lambdaBody | lambdaType | lambdaNext
-    "." ~ conn              ^^ {case _~ c => DSL.lam(_:Var,c)} |
-    identifier ~ lambdaCont ^^ { case v ~ f => DSL.lam(_:Var,f(DSL.str2IVar(v))) } |
+  def lit: Parser[Connector] =
+    bexpr ~ "?" ~ conn ~ "+" ~ conn ^^ {case b~_~c1~_~c2 => Choice(b,c1,c2)}       |
+    "\\" ~ identifier ~ lambdaCont  ^^ {case _~ s ~ cont => cont(DSL.str2IVar(s))} |
+    "(" ~ conn ~ ")"                ^^ {case _ ~ c ~ _ => c}                       |
+    identifier~"="~conn~";"~conn    ^^ {case s~_~c1~_~c2 => Substitution.replacePrim(s,c2,c1)} |
+    identifier                      ^^ { inferPrim }
+
+  def lambdaCont: Parser[Var=>Connector] =
+    "." ~ conn                   ^^ {case _~ c   => DSL.lam(_:Var,c)}                   |
+    identifier ~ lambdaCont      ^^ { case v ~ f => DSL.lam(_:Var,f(DSL.str2IVar(v))) } |
     ":" ~ ("I"|"B") ~ lambdaCont ^^ {
       case _~ "I" ~ cont => (v:Var) => cont(v) // IVar is the default
       case _~ "B" ~ cont => (v:Var) => cont(DSL.str2BVar(v.x))
     }
-//  def lambdaNext:Parser[Var=>Connector] = identifier ~ lambdaCont ^^ {
-//    case v ~ f => DSL.lam(_,f(DSL.str2IVar(v)))
-//  }
-//  def lambdaBody:Parser[Var=>Connector] = "." ~ conn ^^ {case _~ c => DSL.lam(_,c)}
-//  def lambdaType:Parser[Var=>Connector] = ":" ~ ("I"|"B") ~ lambdaCont ^^ {
-//    case _~ "I" ~ cont => (v:Var) => cont(v) // IVar is the default
-//    case _~ "B" ~ cont => (v:Var) => cont(DSL.str2BVar(v.x))
-//  }
-//  //  def lambdCont:Parser[String=>Connector] = lambdaType | body
-//  def brck:Parser[Connector] = "(" ~ conn ~ ")" ^^ { case _ ~ c ~ _ => c }
-
-  // Combinators:
-//  def compBuilder:Parser[Connector=>Connector] = "&" ~ conn   ^^ {case _~ c => _ & c}
-//  def parBuilder: Parser[Connector=>Connector] = "*" ~ conn   ^^ {case _~ c => _ * c}
-//  def expBuilder: Parser[Connector=>Connector] = "^" ~ intExp ^^ {case _~ i => _ ^ i}
-  // simplification - only a single feature in the "if" statement
-//  def ifBuilder:  Parser[Connector=>Connector] = "?" ~ conn ~ "+" ~ conn ^^ {case _~c1 ~ _ ~ c2 => c => Choice(BVar(Show(c)), c1, c2) }
-//  def end:        Parser[Connector=>Connector] = "" ^^ { _ => (x: Connector) => x }
-
-  // Integer expressions
-  def intExp: Parser[IExpr] = intVal | intVar
-  def intVar: Parser[IExpr] = identifier ^^ DSL.str2IVar
-
-  // TODO: missing: conditionals, bool expressions
-
 
   // boolean expressions
   def bexpr: Parser[BExpr] =
     blit ~ bbop ~ bexpr ^^ {case l ~ op ~ r => op(l,r)} |
-      ilit ~ bibop ~ iexpr ^^ {case l ~ op ~ r => op(l,r)} |
-      "!" ~ bexpr ^^ {case _ ~ e => Not(e)} |
-      blit
+    ilit ~ bibop ~ iexpr ^^ {case l ~ op ~ r => op(l,r)} |
+    "!" ~ bexpr ^^ {case _ ~ e => Not(e)} |
+    blit
   def blit: Parser[BExpr] =
-    "true"     ^^ {_=>BVal(true)}  |
-      "false"    ^^ {_=>BVal(false)} |
-      identifier ^^ BVar             |
-      "(" ~ bexpr ~ ")" ^^ {case _ ~ e ~ _ => e }
+    "true"     ^^ {_=>BVal(true)}                |
+    "false"    ^^ {_=>BVal(false)}               |
+    identifier~":"~"B" ^^ {case s~_~_=>BVar(s) } |
+    identifier ^^ BVar                           |
+    "(" ~ bexpr ~ ")" ^^ {case _ ~ e ~ _ => e }
   def bbop: Parser[(BExpr,BExpr)=>BExpr] =
     "&"  ^^ {_ => (e1:BExpr,e2:BExpr) => e1 & e2 } |
-      "|"  ^^ {_ => (e1:BExpr,e2:BExpr) => e1 | e2 } |
-      "<->" ^^ {_ => (e1:BExpr,e2:BExpr) => e1 === e2 }
+    "|"  ^^ {_ => (e1:BExpr,e2:BExpr) => e1 | e2 } |
+    "<->" ^^ {_ => (e1:BExpr,e2:BExpr) => e1 === e2 }
   def bibop: Parser[(IExpr,IExpr)=>BExpr] =
     "<=" ^^ {_ => (e1:IExpr,e2:IExpr) => e1 <= e2 } |
-      ">=" ^^ {_ => (e1:IExpr,e2:IExpr) => e1 >= e2 } |
-      "<"  ^^ {_ => (e1:IExpr,e2:IExpr) => e1 < e2 }  |
-      ">"  ^^ {_ => (e1:IExpr,e2:IExpr) => e1 > e2 }  |
-      "==" ^^ {_ => (e1:IExpr,e2:IExpr) => e1 === e2 }
+    ">=" ^^ {_ => (e1:IExpr,e2:IExpr) => e1 >= e2 } |
+    "<"  ^^ {_ => (e1:IExpr,e2:IExpr) => e1 < e2 }  |
+    ">"  ^^ {_ => (e1:IExpr,e2:IExpr) => e1 > e2 }  |
+    "==" ^^ {_ => (e1:IExpr,e2:IExpr) => e1 === e2 }
 
   // integer expressions
   def iexpr: Parser[IExpr] =
     ilit ~ ibop ~ iexpr ^^ {case l ~ op ~ r => op(l,r)} |
-      ilit
+    ilit
   def ilit: Parser[IExpr] =
-    intVal |
-      identifier ^^ IVar |
-      "(" ~ iexpr ~ ")" ^^ {case _ ~ e ~ _ => e }
+    intVal                                       |
+    identifier~":"~"I" ^^ {case s~_~_=>IVar(s) } |
+    identifier ^^ IVar                           |
+    "(" ~ iexpr ~ ")" ^^ {case _ ~ e ~ _ => e }
   def intVal: Parser[IExpr] =
     """[0-9]+""".r ^^ { (s:String) => DSL.int2IExp(s.toInt) }
   def ibop: Parser[(IExpr,IExpr)=>IExpr] =
     "+"  ^^ {_ => (e1:IExpr,e2:IExpr) => e1 + e2 } |
-      "-"  ^^ {_ => (e1:IExpr,e2:IExpr) => e1 - e2 } |
-      "*"  ^^ {_ => (e1:IExpr,e2:IExpr) => e1 * e2 } |
-      "/"  ^^ {_ => (e1:IExpr,e2:IExpr) => e1 / e2 }
-
+    "-"  ^^ {_ => (e1:IExpr,e2:IExpr) => e1 - e2 } |
+    "*"  ^^ {_ => (e1:IExpr,e2:IExpr) => e1 * e2 } |
+    "/"  ^^ {_ => (e1:IExpr,e2:IExpr) => e1 / e2 }
 
 }
